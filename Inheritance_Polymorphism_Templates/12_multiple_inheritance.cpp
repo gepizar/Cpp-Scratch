bdac6c3679bd47d9068cc2aa888c63414d0122bc
gepizar <gepizarro@uc.cl>
2020-05-17 19:49:19 -0400

    doc: Multiple Inheritance
    Add documentation to the examples

--- Inheritance_Polymorphism_Templates/12_multiple_inheritance.cpp
+++ Inheritance_Polymorphism_Templates/12_multiple_inheritance.cpp
@@ -21,3 +21,16 @@ int main() {
     assert(duck.Drive() == car.Drive());
     assert(duck.Cruise() == boat.Cruise()); 
 }
+
+/* 
+ * If you have class Animal and another class Pet, then you can construct a 
+ * class Dog, which inherits from both of these base classes. In doing this, you 
+ * are able to incorporate attributes of multiple base classes.
+ * 
+ * C.135: Use multiple inheritance to represent multiple distinct interfaces
+ * http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#c135-use-multiple-inheritance-to-represent-multiple-distinct-interfaces
+ * 
+ * C.136: Use multiple inheritance to represent the union of implementation 
+ * attributes
+ * http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#c136-use-multiple-inheritance-to-represent-the-union-of-implementation-attributes
+ */
