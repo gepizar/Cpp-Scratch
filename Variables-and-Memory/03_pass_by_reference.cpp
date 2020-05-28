72f17b225e0c3a6ef736400ea82ac0ee9dab18b6
gepizar <gepizarro@uc.cl>
2020-05-27 22:42:51 -0400

    feat: Pointers/References
    Example of passing variables by value/Pointers/References

---
+++ Variables-and-Memory/03_pass_by_reference.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void AddFour(int &val)
+{
+    val += 4;
+}
+
+int main()
+{
+    int val = 0;
+    AddFour(val);
+    val += 2;
+
+    std::cout << "val = " << val << std::endl;
+ 
+    return 0;
+}
+
+/* 
+ * The second major way of passing parameters to a function is by reference. 
+ * With this way, the function receives a reference to the parameter, rather 
+ * than a copy of its value. As with the example of AddThree above, the function 
+ * can now modify the argument such that the changes also happen on the caller 
+ * side. In addition to the possibility to directly exchange information between 
+ * function and caller, passing variables by reference is also faster as no 
+ * information needs to be copied, as well as more memory-efficient. 
+ * 
+ * A major disadvantage is that the caller does not always know what will happen 
+ * to the data it passes to a function (especially when the function code can 
+ * not be modified easily). Thus, in some cases, special steps must be taken to 
+ * protect ones data from inappropriate modification.
+ * 
+ */
